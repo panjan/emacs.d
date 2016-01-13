a6226d6ad1ac1559885568e6905cd70bfd786484
panjan <panjan@panjan.cz>
2016-01-13 16:02:05 +0100

    rspec-mode: don't use rake

--- init.el
+++ init.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (tango-dark)))
  '(js-indent-level 2)
- '(js2-basic-offset 2))
+ '(js2-basic-offset 2)
+ '(rspec-use-rake-when-possible nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
